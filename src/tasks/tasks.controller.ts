e4953db926d8a2dfe747609ed7fd30f63916d22e
Rodrigo Hernández <onigirimex@gmail.com>
2021-01-19 00:28:21 -0600

    Adding the Delete and Patch methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Task } from './tasks.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 import { CreateTaskDTO } from './dto/create-task-dto';
 
@@ -14,10 +14,25 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   createTask(@Body() createTaskDTO: CreateTaskDTO): Task {
-
     return this.tasksService.createTask(createTaskDTO);
   }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): Task[] | boolean {
+    return this.tasksService.deleteTaskById(id);
+  }
+
+  @Patch('/:id/status')
+  updateStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+    return this.tasksService.updateStatus(id, status);
+  }
+
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  public getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
   public createTask(createTaskDTO: CreateTaskDTO): Task {
     const { title, description } = createTaskDTO; // Sugar Syntax from ES6 that extracts only the keys of the object that we care for
 
@@ -23,6 +27,22 @@ export class TasksService {
 
     this.tasks.push(task);
     
+    return task;
+  }
+
+  public deleteTaskById(id: string): Task[] | boolean{
+    if (this.tasks.find(task => task.id === id)) {
+      this.tasks = this.tasks.filter(el => el.id !== id);
+      return this.tasks;
+    }
+
+    return false;
+  }
+
+  public updateStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+
     return task;
   }
 }
