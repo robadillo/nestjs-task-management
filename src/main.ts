f3813564a641ca062ece7ca3d0cd934c0b355ca2
Rodrigo Hernández <onigirimex@gmail.com>
2021-01-24 04:48:12 -0600

    Enabling cors in order to connect our application to the frontend

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const logger = new Logger('bootstrap'); // Here we instanciate a new logger and indicate the context (in this case, bootstrap)
   const app = await NestFactory.create(AppModule);
 
+  // if (process.env.NODE_ENV === 'development') { //this should work, but the node env is not in development mode, idk why
+    app.enableCors();
+  // }
+  
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
